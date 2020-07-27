3d25831237cfc134a81456adf4817c8cc537b349
sweetpea22 <cindychau24@me.com>
2020-07-27 17:59:14 -0400

    test out libs

---
+++ components/layout.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import styles from "./layout.module.css";
+
+export const siteTitle =
+  "Cadencia - Data Insights for Filecoin Miners and Users";
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="Making it easier for miners and clients to find each other" />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+
+      <main>{children}</main>
+    </div>
+  );
+}
