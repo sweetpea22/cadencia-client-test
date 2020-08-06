4e6fdc739c89eeac0ae48295e6d680087d70cf23
sweetpea22 <cindychau24@me.com>
2020-08-06 15:01:55 -0400

    fix gitignore

---
+++ web/lib/geodata.js
@@ -0,0 +1,35 @@
+const geo = [
+  {
+    peerId: "Wj2LgQXymTopKUff1fmM2QJ6MtAfk1x94NNLqSeD",
+    client: { type: "Lotus", version: "0.2.4" },
+    location: { Lat: 32.8312, Lon: -117.1225 },
+    city: "San Diego",
+    country: "United States",
+    ISP: "Hetzner",
+  },
+
+  {
+    peerId: "Wj2LgQXymTopKUff1fmM2QJ6MtAfk1x94NNLqSeD",
+    client: { type: "Lotus", version: "0.2.4" },
+    location: { Lat: -25.42, Lon: -49.32 },
+    city: "Curitiba",
+    country: "Brazil",
+    ISP: "Hetzner",
+  },
+  {
+    peerId: "Wj2LgQXymTopKUff1fmM2QJ6MtAfk1x94NNLqSeD",
+    client: { type: "Lotus", version: "0.2.4" },
+    location: { Lat: 43.840975, Lon: -79.559635 },
+    city: “Vaughan”,
+    country: "Canada",
+    ISP: "Hetzner",
+      },
+    {
+          peerId: "Wj2LgQXymTopKUff1fmM2QJ6MtAfk1x94NNLqSeD",
+          client: { type: "Lotus", version: "0.2.4" },
+          location: { Lat: 30.67, Lon: 104.07 },
+          city: “Chengdu”,
+          country: “China”,
+          ISP: "Hetzner",
+        },
+];
